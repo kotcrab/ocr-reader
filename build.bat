aa2d74ef9c7751f592ee2ad6cb279426e725459d
kotcrab <4594081+kotcrab@users.noreply.github.com>
2023-01-25 20:38:07 +0100

    Add basic build error check

--- build.bat
+++ build.bat
@@ -1,4 +1,14 @@
 call npm install
+if errorlevel 1 goto :ERR
 call npm run build
+if errorlevel 1 goto :ERR
 echo You can now close this window.
+goto :FIN
+
+:ERR
+echo.
+echo Something went wrong while building. Look at the output above for more details.
+goto :FIN
+
+:FIN
 pause > NUL
